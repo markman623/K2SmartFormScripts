d9210e2dd149135e7246a819c7745046bc97739b
Administrator <Administrator@denallix.com>
2025-04-25 16:50:56 +0200

    files for new video

---
+++ TestNodeSMO.js
@@ -0,0 +1,22 @@
+
+
+fetch("https://k2.denallix.com/Management/Runtime/AjaxCall.ashx", {
+  "headers": {
+    "accept": "application/xml, text/xml, */*, application/json; q=0.01",
+    "accept-language": "en-US,en;q=0.9,ar;q=0.8",
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"99\", \"Microsoft Edge\";v=\"133\", \"Chromium\";v=\"133\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "xsrftoken": "6907242403373582605"
+  },
+  "referrer": "https://k2.denallix.com/Management/Runtime/Form.aspx?_ID=2ae83f85-6336-49f1-a9e5-fe78b220f6c4&methodtoexecute=GetList&hasadminrights=True&smartobjectguid=c4106254-b5e2-4f34-b500-241f500d67a8",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "[{\"smartobject\":{\"guid\":\"30c93355-cd38-4151-9ff7-558aa783691d\",\"resultName\":\"Primary_30c93355-cd38-4151-9ff7-558aa783691d_30c93355-cd38-4151-9ff7-558aa783691d\",\"method\":{\"name\":\"QSInOcNns9DtvuYT7vVUHILjFwpD4Ya8xdUhAkQXBEX1vugsBGq_VDDxuuxgBk8vWvW8rIR-6OcP48L0d3f2_u3Hl4ZZ5fcojmLvA5Abf50ytSJfM2Zh0IGjSEvarT7T0\"},\"properties\":{\"Guid\":{\"value\":\"c4106254-b5e2-4f34-b500-241f500d67a8\"}},\"parameters\":{\"inputproperties__smartproperty___\":{\"value\":\"[{\\\"DisplayName\\\":\\\"ID\\\",\\\"IsRequired\\\":false,\\\"PropertyType\\\":\\\"Autonumber\\\",\\\"Value\\\":\\\"\\\",\\\"Description\\\":\\\"The key used to identify a specific record.\\\",\\\"Name\\\":\\\"ID\\\",\\\"Type\\\":\\\"Property\\\",\\\"Serialized_Array\\\":\\\"\\\"},{\\\"DisplayName\\\":\\\"CountryName\\\",\\\"IsRequired\\\":false,\\\"PropertyType\\\":\\\"Text\\\",\\\"Value\\\":\\\"\\\",\\\"Description\\\":\\\"\\\",\\\"Name\\\":\\\"CountryName\\\",\\\"Type\\\":\\\"Property\\\",\\\"Serialized_Array\\\":\\\"\\\"},{\\\"DisplayName\\\":\\\"CountryCode\\\",\\\"IsRequired\\\":false,\\\"PropertyType\\\":\\\"Text\\\",\\\"Value\\\":\\\"\\\",\\\"Description\\\":\\\"\\\",\\\"Name\\\":\\\"CountryCode\\\",\\\"Type\\\":\\\"Property\\\",\\\"Serialized_Array\\\":\\\"\\\"},{\\\"DisplayName\\\":\\\"PhoneCountryCode\\\",\\\"IsRequired\\\":false,\\\"PropertyType\\\":\\\"Text\\\",\\\"Value\\\":\\\"\\\",\\\"Description\\\":\\\"\\\",\\\"Name\\\":\\\"PhoneCountryCode\\\",\\\"Type\\\":\\\"Property\\\",\\\"Serialized_Array\\\":\\\"\\\"}]\"},\"methodname\":{\"value\":\"GetList\"}},\"results\":[{\"targetID\":\"66eaf885-fdb8-b65b-71eb-f4efa0718d35_67aa1d7a-efc4-4c42-99e5-94871bdda213\",\"targetType\":\"Control\",\"sourceType\":\"ObjectProperty\",\"sourceID\":\"SmartObjectResultRow____SmartObjectResultRow___\",\"sourceInstanceID\":null,\"targetInstanceID\":\"66eaf885-fdb8-b65b-71eb-f4efa0718d35\"}]},\"metadata\":{\"methodexecuted\":\"QSInOcNns9DtvuYT7vVUHILjFwpD4Ya8xdUhAkQXBEX1vugsBGq_VDDxuuxgBk8vWvW8rIR-6OcP48L0d3f2_u3Hl4ZZ5fcojmLvA5Abf50ytSJfM2Zh0IGjSEvarT7T0\",\"typeofview\":\"Capture\",\"objectid\":\"30c93355-cd38-4151-9ff7-558aa783691d\"}}]",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
